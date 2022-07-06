ade99781f93e8e8bf4dbe19730f47d95aeba0685
Hilary Okemeziem <hilaryokems@gmail.com>
2022-07-06 01:36:03 +0100

    production build

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -40,7 +40,7 @@ const Hero = () => {
                     <p className='text-[#ff5757] text-2xl'>Hello I&apos;m</p>
                     <h1 className='text-4xl sm:text-6xl font-bold text-[#ccf6f6]'>Hilary Okemeziem</h1>
                     <h2 className='text-4xl sm:text-6xl font-bold text-[#8892b0]'>A Frontend Developer.</h2>
-                    <p className='text-[#8892b0] py-4 max-w-[700px]'>I&apos;m a frontend developer specializing in building (and occasionally designing) exceptional <br/> digital experiences. Currently, I&apos;m focused on building responsive web applications.</p>
+                    <p className='text-[#8892b0] py-4 max-w-[700px]'>I&apos;m a frontend developer specializing in building (and occasionally designing) exceptional digital experiences. Currently, I&apos;m focused on building responsive web applications.</p>
 
                     <div>
                     <Link href='/#project'>
