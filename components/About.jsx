848790cbe75b29bdac8d5f7260add7986823d24b
Hilary Okemeziem <hilaryokems@gmail.com>
2022-07-07 10:48:21 +0100

    production build

--- components/About.jsx
+++ components/About.jsx
@@ -36,11 +36,11 @@ const About = () => {
                 <p className='py-2 text-gray-400'>I am a Frontend Web Developer who loves making experiences that sticks. My passion for learning and my ability to learn quickly enabled me gain mastery over a wide range of design concepts and technologies.<b> My drive is simple:Build good websites that solves a problem. Also, build websites that people would love to use and experience again.</b></p>
               </Fade>
 
-              <Fade duration={2000} delay={1000} left>
+              <Fade duration={2000} left>
                 <p className='py-2 text-gray-400'>I take pride in my work, and only deliver professional, clean, polished products. Currently a student of the University of Lagos to gain my Bachelors Degree in Applied Physics and Electronics, I learnt frontend web development in Tech Studio Academy for three(3) months where I gained my first certificate in web development. Today, I have more than one(1) year of experience in I.T and Web development.</p>
               </Fade>
 
-              <Fade duration={2000} delay={2000} bottom left>
+              <Fade duration={2000} bottom left>
                 <p className='py-2 text-gray-400 md:hidden lg:block'>I pivoted into I.T because it was fascinating to see how tech products could help individuals and organizations save time, save money and expedite processes. It&apos;s simply magical!</p>
               </Fade>
             </div>
