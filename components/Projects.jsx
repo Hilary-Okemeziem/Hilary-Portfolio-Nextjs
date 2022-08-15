25e3f2d4a6a6bf08705800541804cad20b04bfe8
Hilary Okemeziem <hilaryokems@gmail.com>
2022-08-15 15:34:29 +0100

    added new project

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,8 +4,10 @@ import netflix from '../public/images/netflix.png'
 import crypto from '../public/images/crypto.png'
 import twitch from '../public/images/twitch.png'
 import kaycelle from '../public/images/kaycelle.png'
+import realestate from '../public/images/realestate.png'
 import Image from 'next/image'
 import { Bounce, Fade, Flip, Roll, Slide, Zoom } from 'react-reveal'
+import {AiOutlineEye, AiFillGithub} from 'react-icons/ai'
 
 const Projects = () => {
     function reveal() {
@@ -35,7 +37,7 @@ const Projects = () => {
                 <p className='text-3xl font-bold inline border-b-4 border-[#ff5757] uppercase tracking-widest'>Projects</p>
             </div>
             <h2 className='text-4xl text-gray-400 py-4 font-bold'>What I&apos;ve Built</h2>
-            <div className='grid md:grid-cols-2 gap-8'>
+            <div className='hidden md:grid md:grid-cols-2 gap-8'>
                 <Fade duration={2000} bottom >
                     <div className='relative flex items-center justify-center h-auto w-full shadow-md shadow-black bg-[#0e0e10] rounded-xl p-4 group hover:bg-gradient-to-r from-[#ff5757] to-[#f899ac]'>
                         <Image src={kaycelle} alt="/" className='rounded-xl group-hover:opacity-30' />
@@ -48,6 +50,19 @@ const Projects = () => {
                         </div>
                     </div>
                 </Fade>
+
+                <Slide duration={2000} left >
+                    <div className='relative flex items-center justify-center h-auto w-full shadow-md shadow-black bg-[#0e0e10] rounded-xl p-4 group hover:bg-gradient-to-r from-[#ff5757] to-[#f899ac]'>
+                        <Image src={realestate} alt="/" className='rounded-xl group-hover:opacity-30' />
+                        <div className='hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]'>
+                            <h3 className='text-3xl tracking-wider text-white text-center'>Real Estate Web</h3>
+                            <p className='pb-4 pt-2 text-white text-center'>Next JS</p>
+                            <Link href='/RealEstate'>
+                                <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+                            </Link>
+                        </div>
+                    </div>
+                </Slide>
 
                 <Roll duration={2000}>
                     <div className='relative flex items-center justify-center h-auto w-full shadow-md shadow-black bg-[#0e0e10] rounded-xl p-4 group hover:bg-gradient-to-r from-[#ff5757] to-[#f899ac]'>
@@ -87,8 +102,127 @@ const Projects = () => {
                         </div>
                     </div>
                 </Slide>
+            </div>
 
-                
+
+
+
+
+            <div className='grid sm:grid-cols-2 md:hidden gap-8'>
+                <Fade duration={2000} bottom >
+                    <div className='h-auto w-full bg-[#0e0e10] rounded-b-xl'>
+                        <Image src={kaycelle} alt="/" className='rounded-t-xl'/>
+                        <div className='shadow-md shadow-black bg-[#0e0e10] rounded-b-xl p-2'>
+                            <div className='flex items-center justify-between'>
+                                <h3 className='text-2xl tracking-wider text-white'>Ecommerce Store</h3>
+                                <div className='flex items-center gap-4'>
+                                    <a href="https://kaycelle-brand.vercel.app/" target='_blank' rel='noreferrer'>
+                                        <AiOutlineEye size={22}/>
+                                    </a>
+                                    <a href="https://github.com/Hilary-Okemeziem/Ecommerce-web-reactjs" target='_blank' rel='noreferrer'>
+                                        <AiFillGithub size={22}/>
+                                    </a>
+                                </div>
+                            </div>
+                            <p className='pb-3 pt-1 text-white'>React JS</p>
+                            <Link href='/WebStore'>
+                                <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+                            </Link>
+                        </div>
+                    </div>
+                </Fade>
+
+                <Slide duration={2000} left >
+                    <div className='h-auto w-full bg-[#0e0e10] rounded-b-xl'>
+                        <Image src={realestate} alt="/" className='rounded-t-xl'/>
+                        <div className='shadow-md shadow-black bg-[#0e0e10] rounded-b-xl p-2'>
+                            <div className='flex items-center justify-between'>
+                                <h3 className='text-2xl tracking-wider text-white'>Real Estate Web</h3>
+                                <div className='flex items-center gap-4'>
+                                    <a href="https://lhary-real-estate.vercel.app" target='_blank' rel='noreferrer'>
+                                        <AiOutlineEye size={22}/>
+                                    </a>
+                                    <a href="https://github.com/Hilary-Okemeziem/Real-estate-web-nextjs" target='_blank' rel='noreferrer'>
+                                        <AiFillGithub size={22}/>
+                                    </a>
+                                </div>
+                            </div>
+                            <p className='pb-3 pt-1 text-white'>React JS</p>
+                            <Link href='/WebStore'>
+                                <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+                            </Link>
+                        </div>
+                    </div>
+                </Slide>
+
+                <Zoom duration={2000}>
+                    <div className='h-auto w-full bg-[#0e0e10] rounded-b-xl'>
+                        <Image src={crypto} alt="/" className='rounded-t-xl'/>
+                        <div className='shadow-md shadow-black bg-[#0e0e10] rounded-b-xl p-2'>
+                            <div className='flex items-center justify-between'>
+                                <h3 className='text-2xl tracking-wider text-white'>Crypto App</h3>
+                                <div className='flex items-center gap-4'>
+                                    <a href="https://cryptoexpress-dc4ab.web.app/" target='_blank' rel='noreferrer'>
+                                        <AiOutlineEye size={22}/>
+                                    </a>
+                                    <a href="https://github.com/Hilary-Okemeziem/Crypto-web" target='_blank' rel='noreferrer'>
+                                        <AiFillGithub size={22}/>
+                                    </a>
+                                </div>
+                            </div>
+                            <p className='pb-3 pt-1 text-white'>React JS</p>
+                            <Link href='/WebStore'>
+                                <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+                            </Link>
+                        </div>
+                    </div>
+                </Zoom>
+
+                <Fade duration={2000} top left>
+                    <div className='h-auto w-full bg-[#0e0e10] rounded-b-xl'>
+                        <Image src={netflix} alt="/" className='rounded-t-xl'/>
+                        <div className='shadow-md shadow-black bg-[#0e0e10] rounded-b-xl p-2'>
+                            <div className='flex items-center justify-between'>
+                                <h3 className='text-2xl tracking-wider text-white'>Netflix UI clone</h3>
+                                <div className='flex items-center gap-4'>
+                                    <a href="https://netflix-7e17a.web.app/" target='_blank' rel='noreferrer'>
+                                        <AiOutlineEye size={22}/>
+                                    </a>
+                                    <a href="https://github.com/Hilary-Okemeziem/Netflix" target='_blank' rel='noreferrer'>
+                                        <AiFillGithub size={22}/>
+                                    </a>
+                                </div>
+                            </div>
+                            <p className='pb-3 pt-1 text-white'>React JS</p>
+                            <Link href='/WebStore'>
+                                <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+                            </Link>
+                        </div>
+                    </div>
+                </Fade>
+
+                <Slide duration={2000} bottom >
+                    <div className='h-auto w-full bg-[#0e0e10] rounded-b-xl'>
+                        <Image src={twitch} alt="/" className='rounded-t-xl'/>
+                        <div className='shadow-md shadow-black bg-[#0e0e10] rounded-b-xl p-2'>
+                            <div className='flex items-center justify-between'>
+                                <h3 className='text-2xl tracking-wider text-white'>Twitch UI clone</h3>
+                                <div className='flex items-center gap-4'>
+                                    <a href="https://twitch-nextjs.vercel.app/" target='_blank' rel='noreferrer'>
+                                        <AiOutlineEye size={22}/>
+                                    </a>
+                                    <a href="https://github.com/Hilary-Okemeziem/Twitch-nextjs" target='_blank' rel='noreferrer'>
+                                        <AiFillGithub size={22}/>
+                                    </a>
+                                </div>
+                            </div>
+                            <p className='pb-3 pt-1 text-white'>Next JS</p>
+                            <Link href='/WebStore'>
+                                <p className='text-center p-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer'>More Info</p>
+                            </Link>
+                        </div>
+                    </div>
+                </Slide>   
             </div>
         </div>
     </div>
--- pages/RealEstate.jsx
+++ pages/RealEstate.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import Image from 'next/image'
-import crypto from '../public/images/crypto.png'
+import realestate from '../public/images/realestate.png'
 import {RiRadioButtonFill} from 'react-icons/ri'
 import  Link  from 'next/link'
 
-const Crypto = () => {
+const RealEstate = () => {
   return (
     <div className='w-full bg-[#070708]'>
         <div className='w-screen h-[30vh] lg:h-[50vh] relative'>
             <div className='absolute top-0 left-0 w-full h-[30vh] lg:h-[50vh] bg-black/80 z-10'/>
-            <Image src={crypto} alt="/" className='absolute z-1' layout='fill' objectFit='cover'/>
+            <Image src={realestate} alt="/" className='absolute z-1' layout='fill' objectFit='cover'/>
             <div className='absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-gray-300 z-10 p-2'>
-                <h2 className='py-2 text-4xl'>Crypto App</h2>
-                <h3 className='text-2xl'>React JS / Tailwind / Firebase / Context API</h3>
+                <h2 className='py-2 text-4xl'>Real Estate Website</h2>
+                <h3 className='text-2xl'>Next JS / Tailwind / Firebase</h3>
             </div>
         </div>
 
@@ -20,17 +20,16 @@ const Crypto = () => {
             <div className='col-span-4'>
                 <p className='font-bold'>Projects</p>
                 <h2 className='text-4xl'>Overview</h2>
-                <div className='py-2'>This Crypto App was built with React JS and is hosted on Firebase. It is a frontend project with a firebase backend.</div>
-                <div className='py-2'>I made use of Coin Gecko API for all of the coin data and Firebase for user authentication so users can sign-up and sign-in to their account with an email address in order to save their favorite coins.</div>
-                <div className='py-2'>I made use of Firestore which is cloud storage of firebase to save data, with this users will be able to store/save specific data(coins) from the frontend by clicking the star icon.</div>
-                <div className='py-2'>The project also has an in-built light/dark theme mode which I did using Context API and CSS</div>
+                <div className='py-2'>This Real Estate website was built with NextJS and is hosted on Vercel. It is a frontend project with a firebase backend.</div>
+                <div className='py-2'>I made use of Bayut API from RapidAPI for all of the property data and Firebase for user authentication so users can sign-up and sign-in to their account with an email address in order to save their properties to their wishlist.</div>
+                <div className='py-2'>I made use of Firestore which is cloud storage of firebase to save data, with this users will be able to store/save specific data(property) from the frontend by clicking the heart icon.</div>
                 <div className='py-2'>The project was styled with Tailwind CSS</div>
 
-                <a href="https://cryptoexpress-dc4ab.web.app/" target='_blank' rel='noreferrer'>
+                <a href="https://lhary-real-estate.vercel.app" target='_blank' rel='noreferrer'>
                     <button className='px-8 py-2 mt-4 mr-8 shadow-md shadow-black rounded-xl uppercase bg-gradient-to-r from-[#ff5757] to-[#f899ac] text-gray-100'>Live Demo</button>
                 </a>
 
-                <a href="https://github.com/Hilary-Okemeziem/Crypto-web" target='_blank' rel='noreferrer'>
+                <a href="https://github.com/Hilary-Okemeziem/Real-estate-web-nextjs" target='_blank' rel='noreferrer'>
                     <button className='px-8 py-2 mt-4 mr-8 shadow-md shadow-black rounded-xl uppercase bg-gradient-to-r from-[#ff5757] to-[#f899ac] text-gray-100'>Code</button>
                 </a>
             </div>
@@ -39,13 +38,12 @@ const Crypto = () => {
                     <p className='text-center font-bold pb-2'>Technologies</p>
                 </div>
                 <div className='grid grid-cols-3 md:grid-cols-1'>
-                    <p className='text-gray-400 py-2 flex items-center'><RiRadioButtonFill className='pr-1'/>React</p>
+                    <p className='text-gray-400 py-2 flex items-center'><RiRadioButtonFill className='pr-1'/>NextJS</p>
                     <p className='text-gray-400 py-2 flex items-center'><RiRadioButtonFill className='pr-1'/>CSS</p>
                     <p className='text-gray-400 py-2 flex items-center'><RiRadioButtonFill className='pr-1'/>Javascript</p>
                     <p className='text-gray-400 py-2 flex items-center'><RiRadioButtonFill className='pr-1'/>Tailwind CSS</p>
                     <p className='text-gray-400 py-2 flex items-center'><RiRadioButtonFill className='pr-1'/>Firebase</p>
-                    <p className='text-gray-400 py-2 flex items-center'><RiRadioButtonFill className='pr-1'/>Coin Gecko API</p>
-                    <p className='text-gray-400 py-2 flex items-center'><RiRadioButtonFill className='pr-1'/>Context API</p>
+                    <p className='text-gray-400 py-2 flex items-center'><RiRadioButtonFill className='pr-1'/>Rapid API</p>
                 </div>
             </div>
             <Link href='/#project'>
@@ -56,4 +54,4 @@ const Crypto = () => {
   )
 }
 
-export default Crypto
+export default RealEstate
